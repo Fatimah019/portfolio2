a5b50ffb3e7c6ad53c34d76043800bb3c6a99d57
Fatimah <daviesfatimah97@gmail.com>
2020-07-23 09:46:42 -0700

    links to project

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let about = document.getElementById("about")
+let projectOne = document.getElementById("proj-1")
+let projectTwo = document.getElementById("proj-2")
+let projectThree = document.getElementById("proj-3")
+let projectFour = document.getElementById("proj-4")
+
+function showAbout(){
+    about.href = "components/about/about.html"
+}
+function showPro1(){
+    projectOne.href = "daviesfatimah.netlify.app"
+}
+function showPro2(){
+    projectTwp.href = "foxtrot-schoolfinder.herokuapp.com"
+}
+function showPro3(){
+    projectThree.href = "find-gift.herokuapp.com"
+}
+function showPro4(){
+    projectFour.href = "termsintech.netlify.app"
+}
